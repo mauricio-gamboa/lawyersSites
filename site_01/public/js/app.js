dd1307823657fc830aaf9af676b6cada36fe0cfe
Mauricio Gamboa <magalocr@gmail.com>
2015-07-01 17:35:59 -0600

    Home Partial

--- site_01/public/js/app.js
+++ site_01/public/js/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-var myApp = angular.module('Site_01', []);
+var myApp = angular.module('Site_01', ['ng.deviceDetector']);
+
+myApp.directive('scrollTop', ['$document', function ($document) {
+  return {
+    restrict: 'A',
+
+    link: function () {
+      $($document).scrollTop(0);
+    }
+  };
+}]);
 
 myApp.directive('owlAso', [function () {
   return {
@@ -28,3 +38,28 @@ myApp.directive('owlSingleNav', [function () {
     }
   };
 }]);
+
+myApp.directive('changeMenu', ['deviceDetector', '$window', function (deviceDetector, $window) {
+  return {
+    restrict: 'A',
+
+    link: function (scope, element) {
+      var windowElement = angular.element($window);
+      var $html = $('html');
+      var $use = ((deviceDetector.browser == 'firefox' || deviceDetector.browser == 'ie') ? $html : $body);
+
+      windowElement.on('scroll', function () {
+        if ($use) {
+          var marginTop = element.css('margin-top').replace('px', '') * 1;
+          var marginBottom = element.css('margin-bottom').replace('px', '') * 1;
+          var height = (element.outerHeight() - element.outerHeight()) + marginBottom + marginTop;
+
+          if ($use.scrollTop() > (height))
+            element.addClass('white');
+          else
+            element.removeClass('white');
+        }
+      });
+    }
+  };
+}])
