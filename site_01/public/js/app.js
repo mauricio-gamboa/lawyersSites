c8f0d71976dca3e284a60b13ac6ce4b45bbd3d18
Mauricio Gamboa <magalocr@gmail.com>
2015-07-01 11:27:35 -0600

    Home Partial

---
+++ site_01/public/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var myApp = angular.module('Site_01', []);
+
+myApp.directive('owlAso', [function () {
+  return {
+    restrict: 'A',
+
+    link: function (scope, element) {
+      element.owlCarousel({
+        items: 6,
+        autoPlay: true,
+        pagination: false
+      });
+    }
+  };
+}]);
+
+myApp.directive('owlSingleNav', [function () {
+  return {
+    restrict: 'A',
+
+    link: function (scope, element) {
+      element.owlCarousel({
+        autoPlay: true,
+        singleItem: true
+      });
+    }
+  };
+}]);
